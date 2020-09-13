3b32c150e79bab5586149b712d6d22712c36ff00
Abhishek Kushwaha <thecrazycoderabhi@gmail.com>
2020-09-13 15:05:42 +0530

    made changes for ssh

--- src/utils/ssh_via_subprocess.py
+++ src/utils/ssh_via_subprocess.py
@@ -4,7 +4,7 @@ import asyncio
 from shlex import join, quote
 
 
-async def run_remote_commands_for_data(username, password, host, url_to_curl, port=22):
+async def run_remote_commands_for_data(username, password, host, url_to_curl, port='22'):
     command = "sshpass -p {} ssh -p {} -o StrictHostKeyChecking=no {}@{} 'curl -s {}'".format(quote(password), quote(port), quote(username), quote(host), quote(url_to_curl))
     # string = username + '@' + host
     # #command_1 = 'curl -s ' + url_to_curl
@@ -21,16 +21,16 @@ async def run_remote_commands_for_data(username, password, host, url_to_curl, po
         return {"success":res}
     except Exception as e:
         print("This is error while parsing the output!")
-        print(host," ",e)
+        print(host," ",e, " ", error)
         return {"error": "We have some error in running some commands into your server, please check your creds once, or try checking netdata installation!"}
     else:
         print("This is error while executing!")
         print(host, " " ,error)
         return {"error": "Unable to execute this operation!"}
 
-async def check_valid_ssh_and_netdata(username, password, host, port=22):
+async def check_valid_ssh_and_netdata(username, password, host, port='22'):
     url_to_curl = 'http://localhost:19999/api/v1/info'
-    command = "sshpass -p {} ssh -p {} -o StrictHostKeyChecking=no {}@{} 'curl -s {}'".format(quote(password), quote(port), quote(username), quote(host), quote(url_to_curl))
+    command = "sshpass -p {} ssh -p {} -o StrictHostKeyChecking=no {}@{} 'curl -s {}'".format(quote(password), quote(str(port), quote(username), quote(host), quote(url_to_curl))
     # string = username + '@' + host
     # #command_1 = 'curl -s ' + url_to_curl
     # command = ["sshpass", "-p", password, "ssh", "-p", port, "-o", "StrictHostKeyChecking=no", string,'"curl', '-s', url_to_curl+'"']
@@ -54,8 +54,8 @@ async def check_valid_ssh_and_netdata(username, password, host, port=22):
         print(host, " ", error)
         return {"error": "Unhandled error!"}
 
-async def getting_info_by_command(username, password, host, command, port=22):
-    command = "sshpass -p {} ssh -p {}  -o StrictHostKeyChecking=no {}@{} {}".format(quote(password), quote(port), quote(username), quote(host), quote(command))
+async def getting_info_by_command(username, password, host, command, port='22'):
+    command = "sshpass -p {} ssh -p {}  -o StrictHostKeyChecking=no {}@{} {}".format(quote(password), quote(str(port)), quote(username), quote(host), quote(command))
     
     # string = username + '@' + host
     # command = ["sshpass", "-p", password, "ssh", "-p", port, "-o", "StrictHostKeyChecking=no", string].extend(shlex.split(command))
